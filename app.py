a7331bd72c2f761a83bfcd3f5c9e012b4d223401
Lorenna Aguiar Nunes <133262137+loreslei@users.noreply.github.com>
2025-04-05 11:45:51 -0300

    teste

--- app.py
+++ app.py
@@ -59,7 +59,10 @@ def buscar_operadoras(termo_busca):
 
 @app.route('/', methods=['GET'])
 @app.route('/operadoras', methods=['GET'])
-@cross_origin(origins="https://vue-interface-gamma.vercel.app", methods=['GET'], headers=['Content-Type', 'Authorization'])
+@cross_origin(origins="https://vue-interface-gamma.vercel.app",
+              "https://vue-interface-5nuv06mcs-loresleis-projects.vercel.app"
+)
+, methods=['GET'], headers=['Content-Type', 'Authorization'])
 def listar_operadoras():
     operadoras = ler_todas_operadoras()
     for operadora in operadoras:
@@ -71,7 +74,8 @@ def listar_operadoras():
     return response
 
 @app.route('/operadoras/<termo>', methods=['GET'])
-@cross_origin(origins="https://vue-interface-gamma.vercel.app", methods=['GET'], headers=['Content-Type', 'Authorization'])
+@cross_origin(origins="https://vue-interface-gamma.vercel.app",
+              "https://vue-interface-5nuv06mcs-loresleis-projects.vercel.app", methods=['GET'], headers=['Content-Type', 'Authorization'])
 def buscar(termo):
     if not termo:
         return jsonify({'erro': 'Termo de busca não fornecido'}), 400
